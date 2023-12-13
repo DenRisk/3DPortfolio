9773e69c8cf677dbc34a11af878c209b08771a04
DenRisk <35171305+DenRisk@users.noreply.github.com>
2023-12-13 13:30:18 +0100

    Hero Section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useEffect, useState, Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <div>Computers</div>
   )
